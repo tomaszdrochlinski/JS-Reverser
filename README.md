c5b6a6c489286c988ff06054096ae75d4e3d90ed
Tomasz Drochlinski <tomaszdrochlinski1@gmail.com>
2021-02-28 22:48:33 +0100

    fixed rwd

---
+++ README.md
@@ -0,0 +1,11 @@
+# JS REVERSER
+
+Reverser is a simple application that allows you to reverse the order of the characters in the selected word.
+
+## Technologies
+
+- Vanilla JS
+- SCSS
+
+## Previev
+>https://tomaszdrochlinski.github.io/JS-Reverser/
---
+++ app.js
@@ -0,0 +1,30 @@
+const textInp = document.querySelector('.text-inp');
+const btn = document.querySelector('.btn');
+const disp = document.querySelector('.disp');
+
+const reverseString = (str) => {
+  const arr = new Array();
+  for (let i = str.length - 1; i >= 0; i--) {
+    arr.push(str.charAt(i));
+  }
+
+  let newStr = new String('');
+  arr.forEach((el) => {
+    newStr += el;
+  });
+
+  return newStr;
+};
+
+btn.addEventListener('click', (e) => {
+  const word = new String(textInp.value);
+
+  disp.style.opacity = 0;
+  setTimeout(() => {
+    disp.innerHTML = `${reverseString(word)} &#128565;`;
+  }, 300);
+
+  setTimeout(() => {
+    disp.style.opacity = 1;
+  }, 300);
+});
