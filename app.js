ce6a6153307c1663fb0ba3941a4ec28746876614
Tomasz Drochlinski <tomaszdrochlinski1@gmail.com>
2021-03-01 10:20:27 +0100

    code refactor

--- app.js
+++ app.js
@@ -3,15 +3,11 @@ const btn = document.querySelector('.btn');
 const disp = document.querySelector('.disp');
 
 const reverseString = (str) => {
-  const arr = new Array();
-  for (let i = str.length - 1; i >= 0; i--) {
-    arr.push(str.charAt(i));
-  }
-
   let newStr = new String('');
-  arr.forEach((el) => {
-    newStr += el;
-  });
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    newStr += str.charAt(i);
+  }
 
   return newStr;
 };
